b3f1851bb2eb91a46af6574671a89ef9adcf5487
adam-frfr <adamm.frfr@gmail.com>
2026-06-14 11:56:27 +0530

    Update main.py

--- firmware/main.py
+++ firmware/main.py
@@ -1,1 +1,17 @@
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
 
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.D0, board.D1, board.D2)
+keyboard.row_pins = (board.D3,)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [KC.A, KC.B, KC.C]
+]
+
+if __name__ == "__main__":
+    keyboard.go()
